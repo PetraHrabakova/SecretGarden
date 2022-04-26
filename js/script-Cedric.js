8ff31bf4896a54e83102afdebea2ac87c76bd7b6
The2InchSpecial <tcbcedric@hotmail.com>
2022-04-26 10:15:36 +0200

    Buttefly done

--- js/script-Cedric.js
+++ js/script-Cedric.js
@@ -1,22 +1,23 @@
 //MAKE THE MAGIC HAPPEN
 $(document).ready(function() {
     // Call to function
-    $("#butterfly"),function() {randomMovement(this)};
+    randomMovement("#butterfly");
 });
 
 function randomMovement(IdRef) {
-    var width = $(window).width() - $(this).width();
-    var height = $(window).height() - $(this).height();
-    var randomVertical = Math.floor(Math.random()*height);
-    var randomHorizontal = Math.floor(Math.random()*width);
-    $(IdRef).delay(500).animate({top:randomVertical},"slow").delay(500).animate({left:randomHorizontal},"slow",function(){randomMovement(IdRef)});
+    var width = $(window).width()-$(IdRef).width();//butterfly 300 wide
+    var height = $(window).height()-$(IdRef).height();//butterfly 200 high
+    var randomVertical = Math.random()*height;
+    var randomHorizontal = Math.random()*width;
+    $(IdRef).animate({top:randomVertical},5000).animate({left:randomHorizontal},5000,function(){randomMovement(IdRef)});
 };
 
 $("#butterfly").mouseenter(function(){
+    $("#butterfly").stop(true);
     var width = $(window).width() - $(this).width();
     var height = $(window).height() - $(this).height();
     var randomHeight = Math.floor(Math.random() * height);
     var randomWidth = Math.floor(Math.random() * width);
 
-    $(this).animate({left:randomWidth},"slow").animate({top:randomHeight},"slow",function(){randomMovement("#butterfly")});
+    $(this).animate({left:randomWidth},500).animate({top:randomHeight},500,function(){randomMovement("#butterfly")});
 });
