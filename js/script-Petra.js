34bc7cd900926b3b646ac6396dc41cd9f04a59d8
PetraHrabakova <petrahrab3@gmail.com>
2022-04-26 11:56:50 +0200

    last update

---
+++ js/script-Petra.js
@@ -0,0 +1,20 @@
+// Net
+
+
+var oldX = 0;
+
+
+$(document).mousemove(function(e) {
+    $("#net").css({
+        left: e.pageX,
+        top: e.pageY
+    });
+
+    if (oldX < e.pageX) {
+        $("#net").css('transform', 'rotateY(180deg)');
+
+    } else {
+        $("#net").css('transform', 'none');
+    }
+    oldX = e.pageX;
+});
--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
 //MAKE THE MAGIC HAPPEN
-
-// Net
-$(document).mousemove(function(event) {
-    $("#net").css({
-        left: event.pageX,
-        top: event.pageY
-    });
-});
