92676c436d360e49758bfbf48c6556bc1af0b0f5
VeronicaTverdohleb <101176218+VeronicaTverdohleb@users.noreply.github.com>
2022-04-27 18:28:38 +0200

    drops

--- js/waterscript.js
+++ js/waterscript.js
@@ -12,8 +12,41 @@ $("#wateringcan").click(function () {
 
             }     
         });
-        $('.waterdrop').show();
+        $(".waterdrop").show();
        
+
+       /* $("#drop").animate( "slow" ,function() {downMovement(this)});
+        $(".waterdrop").animate( "slow" ,function() {downMovement(this)});
+        $("#drop3").animate( "slow" ,function() {downMovement(this)});
+
+        function downMovement(IdRef) {
+           
+            
+                $(IdRef).animate({top: "+=100"}, function(){downMovement(IdRef);});  
+            
+        }*/
+        
+        $(".waterdrop").animate( "slow" ,function() {downMovement(this)});
+        $("#drop").animate( "slow" ,function() {downMovement(this)});
+        ("#drop2").animate( "slow" ,function() {downMovement(this)});
+
+        function downMovement(IdRef) {
+         
+             function loop(){
+                 for (var start = 1, end = 3; start < $(".waterdrop").length; start += 3, end += 3){
+           $(".waterdrop")
+           .animate({bottom:-30},1000)
+           .animate({bottom:90},1000, loop);
+           
+    
+           }
+        }
+            loop();  
+        
+        }
+
+        
+        
         toggle=1;
     } else {
         $("#wateringcan").animate({deg: 0}, {
@@ -26,10 +59,10 @@ $("#wateringcan").click(function () {
     }
 });
 
-$(document).ready(function () {
-    $('.waterdrop').hide();
-});
-$('#fade').slideDown("slow");
+//$(document).ready(function () {
+    //$('.waterdrop').hide();
+//});
+//$('#fade').slideDown("slow");
 
 
 
